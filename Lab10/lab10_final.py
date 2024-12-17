7516bcd8772eb9e56fbb66eb1976e590ff990262
Lu Junyou <mushroom.ac.0520@gmail.com>
2024-12-17 20:25:16 +0800

    feat : make tasks as enumerate

--- Lab10/lab10_final.py
+++ Lab10/lab10_final.py
@@ -121,18 +121,14 @@ def load_model():
 def main():
     device, model, doll_names, doll_colors = load_model()
     
-    face1 = False       # default: False(not detected)
-    face2 = False
-    detect_doll = False # default: False(don't detect)
+    tasks = ["face1", "face2", "id1", "line", "before_id2", "id2", "before_id3", "id3"]
+    task = tasks.index("face1")
     line_path = 0       # 0: none, 1: melody(up first), 2: carna(up later)
-    line_trace = False  # default: False(not start)
+    detect_doll = False # default: False(don't detect)
     line_task = 0       # default: 0(not start)
     
-    land = False        # default: False(not start)
     land_melody = False
     land_carna = False
-    line_finish = False
-    finish4 = False
 
     drone = Tello()
     drone.connect()
@@ -175,7 +171,8 @@ def main():
         x_update, y_update, z_update, yaw_update, angle_diff = 0, 0, 0, 0, 0
         
         # Step 1-0 : detect face 
-        if not face1 or not face2 :
+        if task == tasks.index("face1") \
+            or task == tasks.index("face2"):
             frame, face_distance = detect_face(frame, face_objectPoints, intrinsic, distortion)
 
         # Step 2-0, 4-0 : detect doll 
@@ -186,7 +183,8 @@ def main():
               
         # Step 3-0 : line detect 
         state = ""
-        if line_trace:
+        if (task == tasks.index("line") or task == tasks.index("before_id2"))\
+            and drone.is_flying :
             dilation = LT.process_frame(frame)
             binary01 = dilation // 255
             pattern = LT.get_pattern(binary01)
@@ -236,7 +234,8 @@ def main():
 
 
                 #Step 2-2: go to id 1 =================================================================
-                if current_marker_id == 1 and face2 and not line_trace:
+                if current_marker_id == 1 \
+                    and task == tasks.index("id1") :
                     if z is None : ## slowly forward
                         for ii in range (1) :
                             drone.send_rc_control(0, 20, 0, 0)
@@ -254,22 +253,24 @@ def main():
                             stable_counter += 1
                             continue
                         ## go up for line
-                        line_trace = True
+                        task += 1
                         detect_doll = False
                         stable_counter = 0 
                         drone.send_rc_control(0, 5, 30, 0)
                         time.sleep(1.5)
                 
                 # Step 4: 追線完成 ====================================================================                 
-                elif line_finish and current_marker_id == 2: 
-                    line_trace = False
+                elif current_marker_id == 2 \
+                    and (task == tasks.index("id2") or task == tasks.index("before_id2")):
                     detect_doll = True 
                     print("追線完成！正在對準marker2!!!!!!!!!!!!!!!!!!!\n")
                     if z is None : ## slowly forward
+                        task = tasks.index("id2")
                         for ii in range (1) :
                             drone.send_rc_control(0, 20, 0, 0)
                             time.sleep(0.4)
                     if z >= 40 or z <= 32 or y < -10 or 10 < y : # before go right for line
+                        task = tasks.index("id2")
                         z_update += 15 # 50 - 15 cm = 35
                         x_update = thres(x_pid.update(x_update, sleep=0), max_speed)
                         y_update = thres(y_pid.update(y_update, sleep=0), max_speed)
@@ -294,23 +295,23 @@ def main():
                             if doll_name == "carna":
                                 print("偵測到carna CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n")
                                 land_carna = True
-                        finish4 = True
+                        task += 1
                 # Step 5-1: 偵測 carna 或 melody =================================================================                 
-                elif finish4 and land == False : 
+                elif task == tasks.index("before_id3") :
                     if land_carna:
                         print("step 5_1 : CCCCCCCCCCCCCCCCCCCCCC!!!!!!!!!!!\n")
                         drone.send_rc_control(-25, 0, 0, 0) # 向左
                         time.sleep(2)
                         drone.send_rc_control(0, 40, 0, 0) # 向前
                         time.sleep(2)
-                        land = True                              
+                        task += 1                              
                     if land_melody :
                         print("step 5_1 : MMMMMMMMMMMMMMMMMMMMMM!!!!!!!!!!!\n")
                         drone.send_rc_control(25, 0, 0, 0) # 向右
                         time.sleep(2)
                         drone.send_rc_control(0, 40, 0, 0) # 向前
                         time.sleep(2)
-                        land = True
+                        task += 1
                     
                     x_update = thres(x_pid.update(x_update, sleep=0))
                     y_update = thres(y_pid.update(y_update, sleep=0))
@@ -319,7 +320,8 @@ def main():
                     drone.send_rc_control(int(x_update//1), int(z_update//2), int(y_update//1), int(yaw_update//1))
                     
                 # Step 5-2: 偵測 marker id 並降落 =================================================================                 
-                elif land and current_marker_id == 3:
+                elif current_marker_id == 3 \
+                    and task == tasks.index("id3") :
                     print("---------------------marker is 3333333333333333---------------------\n\n\n\n")
                     if z is None : ## slowly forward
                         for ii in range (1) :
@@ -357,7 +359,7 @@ def main():
                 print("PID: {}||{}||{}||{}".format(PID_state["pid_x"],PID_state["pid_y"],PID_state["pid_z"],PID_state["pid_yaw"]))
 
         # Step 1-1: 偵測人臉1 ===============================================================
-        if not face1 and drone.is_flying :
+        if task == tasks.index("face1") and drone.is_flying :
             face1_distance = 70
             
             x_update = face_distance[0]
@@ -367,7 +369,7 @@ def main():
             
             if face_distance[2] > 0 and face_distance[2] < face1_distance :
                 print("偵測到人臉1!!!!!!!!!!!!!!!!!!!!!!!\n")
-                face1 = True
+                task += 1
                 ## up 
                 drone.send_rc_control(0, 0, 50, 0)
                 time.sleep(1.5)
@@ -389,7 +391,7 @@ def main():
                 drone.send_rc_control(0, 0, 30, 0)
                 time.sleep(0.5)
         #Step 1-2: 偵測人臉2 =================================================================
-        elif face1 and not face2 and drone.is_flying :
+        elif task == tasks.index("face2") and drone.is_flying :
             face2_distance = 70
             max_height = 100
             min_height = 20
@@ -404,7 +406,7 @@ def main():
 
             if face_distance[2] > 0 and face_distance[2] < face2_distance :
                 print("偵測到人臉2!!!!!!!!!!!!!!!!!!!!!!!\n")
-                face2 = True
+                task += 1
                 detect_doll = True
                 ## down
                 drone.send_rc_control(0, 0, -50, 0)
@@ -436,7 +438,7 @@ def main():
                     drone.send_rc_control(0, 0, -40, 0)
                     time.sleep(0.5)
         #Step 3-a : line tracing melody(up first) =================================================================
-        elif line_trace and drone.is_flying and line_path == 1 :
+        elif (task == tasks.index("line") or task == tasks.index("before_id2") ) and drone.is_flying and line_path == 1 :
             line_distance = LT.determine_line_distance(frame)
             if line_task == 0 :
                 line_task += 1
@@ -512,11 +514,11 @@ def main():
             elif line_task == 9 :
                 # keep go left 
                 state = LT.horizontal_left_line_tracing(frame, line_distance)
-                line_finish = True
-                ## id2 part should 
-                ### line_trace = False
+                task = tasks.index("before_id2")
+                ## try to find id2
         #Step 3-b : line tracing carna(up later) =================================================================
-        elif line_trace and drone.is_flying and line_path == 2 :
+        elif (task == tasks.index("line") or task == tasks.index("before_id2") )\
+            and drone.is_flying and line_path == 2 :
             line_distance = LT.determine_line_distance(frame)
             if line_task == 0 :
                 line_task += 1
@@ -591,13 +593,13 @@ def main():
             elif line_task == 9 :
                 # keep go left
                 state = LT.horizontal_left_line_tracing(frame, line_distance)
-                line_finish = True
-                ## id2 part should 
-                ### line_trace = False
+                task = tasks.index("before_id2")
+                ## try to find id2 
         #Step 3-c : line tracing exception =================================================================
-        elif line_trace and drone.is_flying :
+        elif task == tasks.index("line") and drone.is_flying :
             print("line tracing exception!!!!!!!!!!!!!!!!!!!!!!!\n")
             print("line_path: ", line_path)
+            print("should be 1(melody) or 2(carna)\n")
             drone.send_rc_control(0, 0, 0, 0)
 
         text = "state: " + state
